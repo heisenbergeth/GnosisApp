60f25183ce515f25ce64d0d1be8ec56c189e69bb
Gem Ryan <gembencito@gmail.com>
2018-09-05 14:56:23 +0800

    Test commit

--- app/src/main/java/com/gnosis/tinder/LoginActivity.java
+++ app/src/main/java/com/gnosis/tinder/LoginActivity.java
@@ -27,7 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+//Test commit
         mAuth = FirebaseAuth.getInstance();
         firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
