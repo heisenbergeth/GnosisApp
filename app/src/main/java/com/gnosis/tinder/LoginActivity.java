afda67f4eac92514983143ba1078b4469db8053d
Mac OS <macos@Gladz.local>
2018-09-06 15:54:02 +0800

    Test commit macOS

--- app/src/main/java/com/gnosis/tinder/LoginActivity.java
+++ app/src/main/java/com/gnosis/tinder/LoginActivity.java
@@ -27,7 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-//test commit 123
+//test commit 12345
         mAuth = FirebaseAuth.getInstance();
         firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
